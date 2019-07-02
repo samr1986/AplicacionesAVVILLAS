c746ec033722c243afdfbfb6f48e04efdc204553
Sergio Andres Melo Ramos <melos@bancoavvillas.com.co>
2019-07-02 10:12:44 -0500

    prueba consumo servicio rest azure

--- models/Invocarservicio.js
+++ models/Invocarservicio.js
@@ -1,11 +1,8 @@
 let request = require('sync-request');
 module.exports.invocarServicio = function(url, path) {
-    console.log("url: " + url);
     let response = request(
         'GET',
         url + path
     );
-    console.log("Status Code (function) : " + response.statusCode);
-    console.log("body : " + response.body);
-    return response;
+    return response.body;
 };
