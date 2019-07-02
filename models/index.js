9767768ddde06aef2a0cbf5c7f8d10aee0fd9594
Sergio Andres Melo Ramos <melos@bancoavvillas.com.co>
2019-07-02 10:57:39 -0500

    prueba consumo servicio rest azure

--- models/index.js
+++ models/index.js
@@ -4,7 +4,7 @@ module.exports.Logueo = function(usu, pass) {
     let metodo = 'GET'
     let url = 'https://ws-rest-creditoempresarial.azurewebsites.net/login?usuario=' + usu + '&password=' + pass;
     let respuestaLogin = InvocaRestService.invocarServicio(metodo, url);
-    return respuestaLogin;
+    return respuestaLogin + 'salida: ' + respuestaLogin.salida;
     /*if (respuestaLogin.salida.codigoRespuesta == 0) {
         return true;
     } else {
