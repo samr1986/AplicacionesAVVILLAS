8bf2cf433a34e24d8fb073822bd53a06c2b9c446
Sergio Andres Melo Ramos <melos@bancoavvillas.com.co>
2019-07-03 10:39:42 -0500

    Consumo servicio rest GET subsanar excepciones

--- models/index.js
+++ models/index.js
@@ -1,6 +1,5 @@
-let InvocaRestService = require("../models/Invocarservicio");
-
 module.exports.Logueo = function(usu, pass) {
+    let InvocaRestService = require("../models/Invocarservicio");
     let metodo = 'GET'
     let url = 'https://ws-rest-creditoempresarial.azurewebsites.net/login?usuario=' + usu + '&password=' + pass;
     let respuestaLogin = InvocaRestService.invocarServicio(metodo, url);
