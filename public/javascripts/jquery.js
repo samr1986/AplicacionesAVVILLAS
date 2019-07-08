6f49d978dcfebb696e385d25a225bb4a6d85f7df
Sergio Andres Melo Ramos <melos@bancoavvillas.com.co>
2019-07-08 14:50:47 -0500

    Consumo servicio rest GET consulta utilizaciones

--- public/javascripts/jquery.js
+++ public/javascripts/jquery.js
@@ -1,5 +1,5 @@
 function onAClick(modalidad, Toperacion, NroUtilizacion) {
-    //$('#EnvioDatos').val(modalidad +',' + Toperacion +',' + NroUtilizacion);
+    $('#EnvioDatos').val(modalidad + ',' + Toperacion + ',' + NroUtilizacion);
     $('#lblmensaje').html(modalidad + ',' + Toperacion + ',' + NroUtilizacion);
     document.getElementById("FormConsultarUtilizaciones").submit();
 }
