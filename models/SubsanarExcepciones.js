a16b76518b4734486c74a5fa2788d988a54a5b8a
Sergio Andres Melo Ramos <melos@bancoavvillas.com.co>
2019-07-08 14:54:19 -0500

    Consumo servicio rest GET consulta utilizaciones

--- models/SubsanarExcepciones.js
+++ models/SubsanarExcepciones.js
@@ -30,10 +30,9 @@ module.exports.FncConsultarExcep = function(ID) {
 module.exports.FncConsultarUtiliza = function(Modalidad, TipoOperacion, NSolicitud) {
     let InvocaRestService = require("../models/Invocarservicio");
     let metodo = 'GET'
-    let url = 'TipoOperacion=' + TipoOperacion + '&NumeroSolicitud=' + NSolicitud + '&Modalidad=' + Modalidad;
-    //let url = 'https://ws-rest-creditoempresarial.azurewebsites.net/utilizaciones?TipoOperacion=' + TipoOperacion + '&NumeroSolicitud=' + NSolicitud + '&Modalidad=' + Modalidad;
-    //let respuestaSubsanarExcepciones = InvocaRestService.invocarServicio(metodo, url);
-    /*if (respuestaSubsanarExcepciones.salida.codigoRespuesta == 0) {
+    let url = 'https://ws-rest-creditoempresarial.azurewebsites.net/utilizaciones?TipoOperacion=' + TipoOperacion + '&NumeroSolicitud=' + NSolicitud + '&Modalidad=' + Modalidad;
+    let respuestaSubsanarExcepciones = InvocaRestService.invocarServicio(metodo, url);
+    if (respuestaSubsanarExcepciones.salida.codigoRespuesta == 0) {
         if (respuestaSubsanarExcepciones.salida.Utilizaciones.length == 1) {
             let ConsultaUtilizacionesSchema = {
                 TipoIdentificacion: respuestaSubsanarExcepciones.salida.Utilizaciones[0].Kl60_tipo_Identificacion,
@@ -162,68 +161,68 @@ module.exports.FncConsultarUtiliza = function(Modalidad, TipoOperacion, NSolicit
             return ConsultaUtilizacionesSchema;
         }
 
-    }*/ // else {
-    let ConsultaUtilizacionesSchema = {
-        TipoIdentificacion: 'url: ' + url,
-        NumeroIdentificacion: '850133065',
-        nombre: 'Empresa xxxx',
-        tamEmpresa: 'CORPORATIVA I',
-        NumeroSolicitud: '',
-        TipoOperacion: '',
-        Modalidad: '',
-        Monto: '10000000',
-        AmortK: '',
-        PerGraK: '',
-        Plazo: '20 dias',
-        AmortI: '',
-        PerGraI: '',
-        Tasa: 'politica + .00%',
-        NPreExecGAP: '0',
-        CodCarte: '0',
-        Estado: 'Desembolsada',
-        ConvFactoring: '0',
-        SerialCartaIntencion: '0',
-        SerialCartaInstruccion: '0',
-        SerialPagare: '9467004958',
-        SerialSegVida: '0',
-        SerialContrato: '0',
-        SerialFNG: '0',
-        ReservaFNG: '0',
-        ConsCodExcep: [{
-            Descripcion: 'Cupo vencido',
-            Estado: 'OK'
-        }, {
-            Descripcion: 'Extracupo por grupo',
-            Estado: 'OK'
-        }, {
-            Descripcion: 'Extracupo por empresa',
-            Estado: 'OK'
-        }, {
-            Descripcion: 'Extracupo por cupo',
-            Estado: 'OK'
-        }, {
-            Descripcion: 'Extracupo por línea',
-            Estado: 'OK'
-        }, {
-            Descripcion: 'Período de gracia capital solicitado > período de gracia aprobado',
-            Estado: 'OK'
-        }, {
-            Descripcion: 'Período de gracia interés solicitado > período de gracia aprobado',
-            Estado: 'OK'
-        }, {
-            Descripcion: 'Plazo solicitado > plazo aprobado',
-            Estado: 'OK'
-        }, {
-            Descripcion: 'Forma pago capital > forma de pago aprobada',
-            Estado: 'OK'
-        }, {
-            Descripcion: 'Forma pago interés > forma de pago aprobada',
-            Estado: 'OK'
-        }, {
-            Descripcion: 'Tasa solicitada < tasa política',
-            Estado: 'OK'
-        }]
-    };
-    return ConsultaUtilizacionesSchema;
-    //}
+    } else {
+        let ConsultaUtilizacionesSchema = {
+            TipoIdentificacion: 'url: ' + url,
+            NumeroIdentificacion: '850133065',
+            nombre: 'Empresa xxxx',
+            tamEmpresa: 'CORPORATIVA I',
+            NumeroSolicitud: '',
+            TipoOperacion: '',
+            Modalidad: '',
+            Monto: '10000000',
+            AmortK: '',
+            PerGraK: '',
+            Plazo: '20 dias',
+            AmortI: '',
+            PerGraI: '',
+            Tasa: 'politica + .00%',
+            NPreExecGAP: '0',
+            CodCarte: '0',
+            Estado: 'Desembolsada',
+            ConvFactoring: '0',
+            SerialCartaIntencion: '0',
+            SerialCartaInstruccion: '0',
+            SerialPagare: '9467004958',
+            SerialSegVida: '0',
+            SerialContrato: '0',
+            SerialFNG: '0',
+            ReservaFNG: '0',
+            ConsCodExcep: [{
+                Descripcion: 'Cupo vencido',
+                Estado: 'OK'
+            }, {
+                Descripcion: 'Extracupo por grupo',
+                Estado: 'OK'
+            }, {
+                Descripcion: 'Extracupo por empresa',
+                Estado: 'OK'
+            }, {
+                Descripcion: 'Extracupo por cupo',
+                Estado: 'OK'
+            }, {
+                Descripcion: 'Extracupo por línea',
+                Estado: 'OK'
+            }, {
+                Descripcion: 'Período de gracia capital solicitado > período de gracia aprobado',
+                Estado: 'OK'
+            }, {
+                Descripcion: 'Período de gracia interés solicitado > período de gracia aprobado',
+                Estado: 'OK'
+            }, {
+                Descripcion: 'Plazo solicitado > plazo aprobado',
+                Estado: 'OK'
+            }, {
+                Descripcion: 'Forma pago capital > forma de pago aprobada',
+                Estado: 'OK'
+            }, {
+                Descripcion: 'Forma pago interés > forma de pago aprobada',
+                Estado: 'OK'
+            }, {
+                Descripcion: 'Tasa solicitada < tasa política',
+                Estado: 'OK'
+            }]
+        };
+        return ConsultaUtilizacionesSchema;
+    }
 };
