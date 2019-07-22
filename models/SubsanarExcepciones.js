0133be77e0c7455fce0d62bb9fc21fe125c30e86
Sergio Andres Melo Ramos <melos@bancoavvillas.com.co>
2019-07-22 17:24:21 -0500

    Consumo servicio rest GET tabla de descripcion de excepcionesempresariales

--- models/SubsanarExcepciones.js
+++ models/SubsanarExcepciones.js
@@ -103,8 +103,7 @@ module.exports.FncConsultarUtiliza = function(Modalidad, TipoOperacion, NSolicit
             ConsultaUtilizacionesSchema.ConsCodExcep = [{
                 //Descripcion: 'Cupo vencido',
                 //Descripcion: JSON.stringify(jsonQuery('ExcepcionesEmpresariales[Codigo_Excepcion=1].Descripcion', { data: respuestaExcepcionesEmpresariales.ExcepcionesEmpresariales }).value),
-                //Descripcion: JSON.stringify(jsonQuery('ExcepcionesEmpresariales[Codigo_Excepcion=1].Descripcion', { data: respuestaExcepcionesEmpresariales.ExcepcionesEmpresariales }).value),
-                Descripcion: JSON.stringify(respuestaExcepcionesEmpresariales.ExcepcionesEmpresariales),
+                Descripcion: JSON.stringify(respuestaExcepcionesEmpresariales),
                 Estado: respuestaSubsanarExcepciones.salida.Utilizaciones[0].IND_EXCEP_VENCIMIENTO
             }, {
                 Descripcion: 'Extracupo por grupo',
