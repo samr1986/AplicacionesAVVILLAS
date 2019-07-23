eb39fbf3bba9531cfbfd05d7a9cce6bf55ff488b
Sergio Andres Melo Ramos <melos@bancoavvillas.com.co>
2019-07-23 08:05:26 -0500

    Consumo servicio rest GET tabla de descripcion de excepcionesempresariales

--- models/SubsanarExcepciones.js
+++ models/SubsanarExcepciones.js
@@ -102,98 +102,127 @@ module.exports.FncConsultarUtiliza = function(Modalidad, TipoOperacion, NSolicit
             ConsultaUtilizacionesSchema.ReservaFNG = respuestaSubsanarExcepciones.salida.Utilizaciones[0].RESERVA_FNG;
             ConsultaUtilizacionesSchema.ConsCodExcep = [{
                 //Descripcion: 'Cupo vencido',
-                Descripcion: JSON.stringify(jsonQuery('ExcepcionesEmpresariales[Codigo_Excepcion=1].Descripcion', { data: respuestaExcepcionesEmpresariales.salida }).value),
-                //Descripcion: JSON.stringify(respuestaExcepcionesEmpresariales),
+                Descripcion: jsonQuery('ExcepcionesEmpresariales[Codigo_Excepcion=1].Descripcion', { data: respuestaExcepcionesEmpresariales.salida }).value,
                 Estado: respuestaSubsanarExcepciones.salida.Utilizaciones[0].IND_EXCEP_VENCIMIENTO
             }, {
-                Descripcion: 'Extracupo por grupo',
+                //Descripcion: 'Extracupo por grupo',
+                Descripcion: jsonQuery('ExcepcionesEmpresariales[Codigo_Excepcion=2].Descripcion', { data: respuestaExcepcionesEmpresariales.salida }).value,
                 Estado: respuestaSubsanarExcepciones.salida.Utilizaciones[0].IND_EXCEP_LEA_GRUPO
             }, {
-                Descripcion: 'Extracupo por empresa',
+                //Descripcion: 'Extracupo por empresa',
+                Descripcion: jsonQuery('ExcepcionesEmpresariales[Codigo_Excepcion=3].Descripcion', { data: respuestaExcepcionesEmpresariales.salida }).value,
                 Estado: respuestaSubsanarExcepciones.salida.Utilizaciones[0].IND_EXCEP_LEA_EMPRESA
             }, {
-                Descripcion: 'Extracupo por cupo',
+                //Descripcion: 'Extracupo por cupo',
+                Descripcion: jsonQuery('ExcepcionesEmpresariales[Codigo_Excepcion=4].Descripcion', { data: respuestaExcepcionesEmpresariales.salida }).value,
                 Estado: respuestaSubsanarExcepciones.salida.Utilizaciones[0].IND_EXCEP_LEA_CUPO
             }, {
-                Descripcion: 'Extracupo por línea',
+                //Descripcion: 'Extracupo por línea',
+                Descripcion: jsonQuery('ExcepcionesEmpresariales[Codigo_Excepcion=5].Descripcion', { data: respuestaExcepcionesEmpresariales.salida }).value,
                 Estado: respuestaSubsanarExcepciones.salida.Utilizaciones[0].IND_EXCEP_LEA_LINEA
             }, {
-                Descripcion: 'Período de gracia capital solicitado > período de gracia aprobado',
+                //Descripcion: 'Período de gracia capital solicitado > período de gracia aprobado',
+                Descripcion: jsonQuery('ExcepcionesEmpresariales[Codigo_Excepcion=6].Descripcion', { data: respuestaExcepcionesEmpresariales.salida }).value,
                 Estado: respuestaSubsanarExcepciones.salida.Utilizaciones[0].IND_EXCEP_GRACIA_K
             }, {
-                Descripcion: 'Período de gracia interés solicitado > período de gracia aprobado',
+                //Descripcion: 'Período de gracia interés solicitado > período de gracia aprobado',
+                Descripcion: jsonQuery('ExcepcionesEmpresariales[Codigo_Excepcion=7].Descripcion', { data: respuestaExcepcionesEmpresariales.salida }).value,
                 Estado: respuestaSubsanarExcepciones.salida.Utilizaciones[0].IND_EXCEP_GRACIA_I
             }, {
-                Descripcion: 'Plazo solicitado > plazo aprobado',
+                //Descripcion: 'Plazo solicitado > plazo aprobado',
+                Descripcion: jsonQuery('ExcepcionesEmpresariales[Codigo_Excepcion=8].Descripcion', { data: respuestaExcepcionesEmpresariales.salida }).value,
                 Estado: respuestaSubsanarExcepciones.salida.Utilizaciones[0].IND_EXCEP_PLAZO
             }, {
-                Descripcion: 'Forma pago capital > forma de pago aprobada',
+                //Descripcion: 'Forma pago capital > forma de pago aprobada',
+                Descripcion: jsonQuery('ExcepcionesEmpresariales[Codigo_Excepcion=9].Descripcion', { data: respuestaExcepcionesEmpresariales.salida }).value,
                 Estado: respuestaSubsanarExcepciones.salida.Utilizaciones[0].IND_EXCEP_AMORTIZ_K1
             }, {
-                Descripcion: 'Forma pago interés > forma de pago aprobada',
+                //Descripcion: 'Forma pago interés > forma de pago aprobada',
+                Descripcion: jsonQuery('ExcepcionesEmpresariales[Codigo_Excepcion=10].Descripcion', { data: respuestaExcepcionesEmpresariales.salida }).value,
                 Estado: respuestaSubsanarExcepciones.salida.Utilizaciones[0].IND_EXCEP_AMORTIZ_I1
             }, {
-                Descripcion: 'Tasa solicitada < tasa política',
+                //Descripcion: 'Tasa solicitada < tasa política',
+                Descripcion: jsonQuery('ExcepcionesEmpresariales[Codigo_Excepcion=11].Descripcion', { data: respuestaExcepcionesEmpresariales.salida }).value,
                 Estado: respuestaSubsanarExcepciones.salida.Utilizaciones[0].IND_EXCEP_TASA
             }, {
-                Descripcion: 'Garantía diferente a hipoteca incompleta o inconsistente',
+                //Descripcion: 'Garantía diferente a hipoteca incompleta o inconsistente',
+                Descripcion: jsonQuery('ExcepcionesEmpresariales[Codigo_Excepcion=12].Descripcion', { data: respuestaExcepcionesEmpresariales.salida }).value,
                 Estado: respuestaSubsanarExcepciones.salida.Utilizaciones[0].IND_EXC_OTRAS_GARANT
             }, {
-                Descripcion: 'Pagaré mal diligenciado o incompleto',
+                //Descripcion: 'Pagaré mal diligenciado o incompleto',
+                Descripcion: jsonQuery('ExcepcionesEmpresariales[Codigo_Excepcion=13].Descripcion', { data: respuestaExcepcionesEmpresariales.salida }).value,
                 Estado: respuestaSubsanarExcepciones.salida.Utilizaciones[0].IND_EXC_PAGARE
             }, {
-                Descripcion: 'Forma pago capital > forma pago aprobada',
+                //Descripcion: 'Forma pago capital > forma pago aprobada',
+                Descripcion: jsonQuery('ExcepcionesEmpresariales[Codigo_Excepcion=14].Descripcion', { data: respuestaExcepcionesEmpresariales.salida }).value,
                 Estado: respuestaSubsanarExcepciones.salida.Utilizaciones[0].IND_EXCEP_AMORTIZ_K2
             }, {
-                Descripcion: 'Forma pago interés > forma pago aprobada',
+                //Descripcion: 'Forma pago interés > forma pago aprobada',
+                Descripcion: jsonQuery('ExcepcionesEmpresariales[Codigo_Excepcion=15].Descripcion', { data: respuestaExcepcionesEmpresariales.salida }).value,
                 Estado: respuestaSubsanarExcepciones.salida.Utilizaciones[0].IND_EXCEP_AMORTIZ_I2
             }, {
-                Descripcion: 'Garantía hipotecaria con boleta pendiente de registro',
+                //Descripcion: 'Garantía hipotecaria con boleta pendiente de registro',
+                Descripcion: jsonQuery('ExcepcionesEmpresariales[Codigo_Excepcion=16].Descripcion', { data: respuestaExcepcionesEmpresariales.salida }).value,
                 Estado: respuestaSubsanarExcepciones.salida.Utilizaciones[0].IND_EXC_GARANT_HIPOT
             }, {
-                Descripcion: 'Falta póliza seguro incendio y terremoto',
+                //Descripcion: 'Falta póliza seguro incendio y terremoto',
+                Descripcion: jsonQuery('ExcepcionesEmpresariales[Codigo_Excepcion=17].Descripcion', { data: respuestaExcepcionesEmpresariales.salida }).value,
                 Estado: respuestaSubsanarExcepciones.salida.Utilizaciones[0].IND_EXC_SEGURO_IT
             }, {
-                Descripcion: 'Falta póliza seguro vida',
+                //Descripcion: 'Falta póliza seguro vida',
+                Descripcion: jsonQuery('ExcepcionesEmpresariales[Codigo_Excepcion=18].Descripcion', { data: respuestaExcepcionesEmpresariales.salida }).value,
                 Estado: respuestaSubsanarExcepciones.salida.Utilizaciones[0].IND_EXC_SEGURO_VIDA
             }, {
-                Descripcion: 'Vigencia de la sociedad < fecha corte + plazo',
+                //Descripcion: 'Vigencia de la sociedad < fecha corte + plazo',
+                Descripcion: jsonQuery('ExcepcionesEmpresariales[Codigo_Excepcion=19].Descripcion', { data: respuestaExcepcionesEmpresariales.salida }).value,
                 Estado: respuestaSubsanarExcepciones.salida.Utilizaciones[0].IND_EXC_VIGENCIA_SOC
             }, {
-                Descripcion: 'Falta acta de junta o se exceden las facultades',
+                //Descripcion: 'Falta acta de junta o se exceden las facultades',
+                Descripcion: jsonQuery('ExcepcionesEmpresariales[Codigo_Excepcion=20].Descripcion', { data: respuestaExcepcionesEmpresariales.salida }).value,
                 Estado: respuestaSubsanarExcepciones.salida.Utilizaciones[0].IND_EXC_ACTA_JUNTAS
             }, {
-                Descripcion: 'Carta de instrucciones mal diligenciada o incompleta',
+                //Descripcion: 'Carta de instrucciones mal diligenciada o incompleta',
+                Descripcion: jsonQuery('ExcepcionesEmpresariales[Codigo_Excepcion=21].Descripcion', { data: respuestaExcepcionesEmpresariales.salida }).value,
                 Estado: respuestaSubsanarExcepciones.salida.Utilizaciones[0].IND_EXC_CARTA_INSTRUC
             }, {
-                Descripcion: 'Falta documento especial',
+                //Descripcion: 'Falta documento especial',
+                Descripcion: jsonQuery('ExcepcionesEmpresariales[Codigo_Excepcion=22].Descripcion', { data: respuestaExcepcionesEmpresariales.salida }).value,
                 Estado: respuestaSubsanarExcepciones.salida.Utilizaciones[0].IND_EXC_DOCUMENTO_ESP
             }, {
-                Descripcion: 'Falta estatutos',
+                //Descripcion: 'Falta estatutos',
+                Descripcion: jsonQuery('ExcepcionesEmpresariales[Codigo_Excepcion=23].Descripcion', { data: respuestaExcepcionesEmpresariales.salida }).value,
                 Estado: respuestaSubsanarExcepciones.salida.Utilizaciones[0].IND_EXC_ESTATUTOS
             }, {
-                Descripcion: 'Falta certificado vigencia sobre acta de junta',
+                //Descripcion: 'Falta certificado vigencia sobre acta de junta',
+                Descripcion: jsonQuery('ExcepcionesEmpresariales[Codigo_Excepcion=24].Descripcion', { data: respuestaExcepcionesEmpresariales.salida }).value,
                 Estado: respuestaSubsanarExcepciones.salida.Utilizaciones[0].IND_EXC_VIGENCIA_ACTA
             }, {
-                Descripcion: 'Carta de intención mal diligenciada o incompleta',
+                //Descripcion: 'Carta de intención mal diligenciada o incompleta',
+                Descripcion: jsonQuery('ExcepcionesEmpresariales[Codigo_Excepcion=25].Descripcion', { data: respuestaExcepcionesEmpresariales.salida }).value,
                 Estado: respuestaSubsanarExcepciones.salida.Utilizaciones[0].IND_EXC_CARTA_INTENCION
             }, {
-                Descripcion: 'CERL vencido (mas de 40 días de expedido)',
+                //Descripcion: 'CERL vencido (mas de 40 días de expedido)',
+                Descripcion: jsonQuery('ExcepcionesEmpresariales[Codigo_Excepcion=26].Descripcion', { data: respuestaExcepcionesEmpresariales.salida }).value,
                 Estado: respuestaSubsanarExcepciones.salida.Utilizaciones[0].IND_EXC_VTO_CERL
             }, {
-                Descripcion: 'Cliente con Excepciones pendientes por subsanar',
+                //Descripcion: 'Cliente con Excepciones pendientes por subsanar',
+                Descripcion: jsonQuery('ExcepcionesEmpresariales[Codigo_Excepcion=27].Descripcion', { data: respuestaExcepcionesEmpresariales.salida }).value,
                 Estado: respuestaSubsanarExcepciones.salida.Utilizaciones[0].IND_EXC_CARTA_INST_CODEU
             }, {
-                Descripcion: 'Visto Bueno Comercial por Montos Inferiores',
+                //Descripcion: 'Visto Bueno Comercial por Montos Inferiores',
+                Descripcion: jsonQuery('ExcepcionesEmpresariales[Codigo_Excepcion=28].Descripcion', { data: respuestaExcepcionesEmpresariales.salida }).value,
                 Estado: respuestaSubsanarExcepciones.salida.Utilizaciones[0].IND_EXC_28
             }, {
-                Descripcion: 'Visto Bueno Comercial por Montos Superiores',
+                //Descripcion: 'Visto Bueno Comercial por Montos Superiores',
+                Descripcion: jsonQuery('ExcepcionesEmpresariales[Codigo_Excepcion=29].Descripcion', { data: respuestaExcepcionesEmpresariales.salida }).value,
                 Estado: respuestaSubsanarExcepciones.salida.Utilizaciones[0].IND_EXC_29
             }, {
-                Descripcion: 'Visto Bueno Credito',
+                //Descripcion: 'Visto Bueno Credito',
+                Descripcion: jsonQuery('ExcepcionesEmpresariales[Codigo_Excepcion=30].Descripcion', { data: respuestaExcepcionesEmpresariales.salida }).value,
                 Estado: respuestaSubsanarExcepciones.salida.Utilizaciones[0].IND_EXC_30
             }, {
-                Descripcion: 'Contrato y/o Convenio mal diligenciado o incompleto',
+                //Descripcion: 'Contrato y/o Convenio mal diligenciado o incompleto',
+                Descripcion: jsonQuery('ExcepcionesEmpresariales[Codigo_Excepcion=31].Descripcion', { data: respuestaExcepcionesEmpresariales.salida }).value,
                 Estado: respuestaSubsanarExcepciones.salida.Utilizaciones[0].IND_EXC_31
             }, ];
             ConsultaUtilizacionesSchema.UsuarioGarHip = respuestaSubsanarExcepciones.salida.Utilizaciones[0].USUARIO_HIP;
